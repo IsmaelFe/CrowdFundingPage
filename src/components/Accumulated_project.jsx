6ec75add18d87bcc872d5d755b591c4ade8d5620
Abner Ismael Sul Felipe <abnersul80@gmail.com>
2023-07-11 19:18:25 -0600

    Componente Accumulated

--- src/components/Accumulated_project.jsx
+++ src/components/Accumulated_project.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const Accumulated_project = () => {
-  return <div>Accumulated_project</div>;
+  return (
+    <div className="container-accumulated">
+      <div className="container-accumulated-1">
+        <div className="container-number">
+          <h3>$89,914</h3>
+          <p>of $100,000 backed</p>
+        </div>
+        <hr></hr>
+        <div className="container-number">
+          <h3>5,007</h3>
+          <p>total backers</p>
+        </div>
+        <hr></hr>
+        <div className="container-number">
+          <h3>56</h3>
+          <p>days left</p>
+        </div>
+      </div>
+      <input className="range" type="range"></input>
+    </div>
+  );
 };
 
 export default Accumulated_project;
