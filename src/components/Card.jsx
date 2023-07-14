edc8e2b59b900062f312c627954ccfcdcf3307b9
Abner Ismael Sul Felipe <abnersul80@gmail.com>
2023-07-13 18:47:05 -0600

    Componente Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Card = () => {
-  return <div>Card</div>;
+  let tittle = "Bamboo Stand";
+  let span = "Pledge $25 or more";
+  let text = "texto";
+  let number = 101;
+  let textButton = "Select Removed";
+
+  return (
+    <div className="container-card-1">
+      <div className="container-card-tittle">
+        <h4>{tittle}</h4>
+        <span className="pledge">{span}</span>
+      </div>
+      <p>{text}</p>
+      <div className="container-card-button">
+        <span className="number">
+          {number}
+          <span className="number-text">{number}</span>
+        </span>
+        <button className="select">{textButton}</button>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
