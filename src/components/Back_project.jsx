53d62cb38920d34512ca659632250c98faf268db
Abner Ismael Sul Felipe <abnersul80@gmail.com>
2023-08-28 18:50:07 -0600

    Imagenes publicas

--- src/components/Back_project.jsx
+++ src/components/Back_project.jsx
@@ -6,7 +6,7 @@ const Back_project = ({ change, changeM, rezise }) => {
     <div className="container-back">
       <img
         className="logo-mastercraft"
-        src="../../public/logo-mastercraft.svg"
+        src="/public/logo-mastercraft.svg"
         alt="logo"
       ></img>
       <div className="container-back-1">
